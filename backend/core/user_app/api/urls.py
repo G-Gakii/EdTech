dc1e9e89cc0b36c5beec5b0f930335e0d76d3244
“G-Gakii” <gakiiviolet1@gmail.com>
2025-05-13 13:44:37 +0300

    add refresh token url

--- backend/core/user_app/api/urls.py
+++ backend/core/user_app/api/urls.py
@@ -4,6 +4,9 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     
 )
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,  # ← import refresh view
+)
 
 urlpatterns = [
     path("register/",RegisterUser.as_view(),name="register"),
@@ -11,5 +14,6 @@ urlpatterns = [
     path("resend-otp/",ResendOtp.as_view(),name='resend-otp'),
     path("login/",CustomTokenObtainPairView.as_view(), name='login'),
     path("reset-password/",ResetPassword.as_view(),name="reset-password"),
-    path("logout/",Logout.as_view(),name="logout")
+    path("logout/",Logout.as_view(),name="logout"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"), 
 ]
